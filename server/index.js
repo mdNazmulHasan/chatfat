431cb49d0371b623373013888f7e030b9ba2266c
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-11-08 12:22:36 +0600

    join component designed

--- server/index.js
+++ server/index.js
@@ -7,5 +7,12 @@ const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", socket => {
+    console.log("We have a new connection!!!!!");
+    socket.on("disconnect", () => {
+        console.log("User has left");
+    });
+});
 app.use(router);
 server.listen(PORT, () => console.log(`Server is started on port:${PORT}`));
