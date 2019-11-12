10fe035c453bd6ee8bd02b1626fe8d0a0ab471d6
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-11-12 12:20:20 +0600

    chat msg submit is coming fine

--- server/index.js
+++ server/index.js
@@ -12,21 +12,33 @@ const io = socketio(server);
 io.on("connection", socket => {
     console.log("We have a new connection!!!!!");
     socket.on("join", ({ name, room }, callback) => {
+        console.log(socket.id);
         const { error, user } = addUser({ id: socket.id, name, room });
+        console.log(user);
+
         if (error) return callback(error);
+
+        socket.join(user.room);
+
         socket.emit("message", {
             user: "admin",
-            text: `${user.name}, welcome to the room ${user.room}`
+            text: `${user.name}, welcome to  room ${user.room}`
         });
-        socket.broadcast
-            .to(user.room)
-            .emit("message", {
-                user: "admin",
-                text: `${user.name}, has joined `
-            });
-        socket.join(user.room);
+        socket.broadcast.to(user.room).emit("message", {
+            user: "admin",
+            text: `${user.name}, has joined `
+        });
         callback();
     });
+
+    socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit("message", { user: user.name, text: message });
+
+        callback();
+    });
+
     socket.on("disconnect", () => {
         console.log("User has left");
     });
